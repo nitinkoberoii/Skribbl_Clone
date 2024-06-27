1c9003a1bd26b014f170f82c6218e52267f1bebc
nitinkoberoii <lonewolfindia15@gmail.com>
2024-06-28 00:07:22 +0530

    Updated these files

--- lib/create_room_screen.dart
+++ lib/create_room_screen.dart
@@ -15,31 +15,6 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
   final TextEditingController _roomNameController = TextEditingController();
   late String? _maxRoundsValue;
   late String? _maxRoomSizeValue;
-  // late IO.Socket socket;
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   connectSocket();
-  // }
-  //
-  // void connectSocket() {
-  //   socket = IO.io('http://192.168.137.1:3000', <String, dynamic>{
-  //     'transports': ['websocket'],
-  //     'autoConnect': false,
-  //   });
-  //   socket.connect();
-  //   socket.onConnect((_) {
-  //     print('connected to server with socket id: ${socket.id}');
-  //   });
-  //   socket.on('notCorrectGame', (data) {
-  //     print('Error: $data');
-  //   });
-  //   socket.on('updateRoom', (data) {
-  //     print('Room updated: $data');
-  //   });
-  //   socket.onDisconnect((_) => print('disconnected'));
-  // }
 
   void createRoom() {
     if (_nameController.text.isNotEmpty &&
@@ -52,8 +27,6 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
         'occupancy': _maxRoomSizeValue!,
         'maxRounds': _maxRoundsValue!,
       };
-      print('Emitting create-game event with data: $data');
-      // socket.emit('create-game', data);
       Navigator.of(context).push(
         MaterialPageRoute(
             builder: (builder) => PaintScreen(
